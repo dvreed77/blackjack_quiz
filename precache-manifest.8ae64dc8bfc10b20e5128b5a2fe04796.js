807b9168fb708c01b0f5df15670b25b46ad89b8e
Dave Reed <dave.reed@thomsonreuters.com>
2018-11-26 10:02:15 -0800

    Updates

---
+++ precache-manifest.8ae64dc8bfc10b20e5128b5a2fe04796.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af90584b286279b27d05",
+    "url": "/blackjack_quiz/static/css/main.9fbe6ecc.chunk.css"
+  },
+  {
+    "revision": "af90584b286279b27d05",
+    "url": "/blackjack_quiz/static/js/main.af90584b.chunk.js"
+  },
+  {
+    "revision": "c85e00e1db5d3bd93d86",
+    "url": "/blackjack_quiz/static/js/1.c85e00e1.chunk.js"
+  },
+  {
+    "revision": "f33d122982f4719eab48",
+    "url": "/blackjack_quiz/static/js/runtime~main.f33d1229.js"
+  },
+  {
+    "revision": "29783d3b888a04dd18b29ed6f6243324",
+    "url": "/blackjack_quiz/index.html"
+  }
+];
